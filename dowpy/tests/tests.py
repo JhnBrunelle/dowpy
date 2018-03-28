b5f0cc0ad15fa1a9adea00ea86d23b314ae6faf4
John Brunelle <Johnzors@gmail.com>
2018-03-28 11:05:33 -0400

    added structure for tests

---
+++ dowpy/tests/tests.py
@@ -0,0 +1,9 @@
+# from unittest import TestCase
+
+# import dowpy
+
+# class TestDownload(TestCase):
+#     def test_pass(self):
+#         s = dowpy.down()
+#		  s -> compare bytes
+#         self.assertTrue(isinstance(s, basestring))
