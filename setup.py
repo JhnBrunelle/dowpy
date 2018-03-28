9cc8ea0ba8a1a03c30c5ca64d813312d46bf84e0
John Brunelle <Johnzors@gmail.com>
2018-03-28 10:55:38 -0400

    setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dowpy',
+      version='0.1',
+      description='Module for downloading HTTP(s) files efficiently',
+      url='http://github.com/jhnbrunelle/dowpy',
+      author='JohnBrunelle',
+      author_email='devjohnb@gmail.com',
+      license='MIT',
+      packages=['dowpy'],
+      zip_safe=False)
