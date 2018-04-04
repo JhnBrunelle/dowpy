86d31bfc0af46826eff222f8c79600cd6022ebf1
John Brunelle <Johnzors@gmail.com>
2018-04-04 13:13:03 -0400

    added readme.rst for pip

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 from setuptools import setup
 
-setup(name='dowpy',
-      version='0.1',
+
+try:
+    long_description = open("README.md").read()
+except IOError:
+    long_description = ""
+
+
+setup(name='DowPy',
+      version='0.1.1',
       description='Module for downloading HTTP(s) files efficiently',
       url='http://github.com/jhnbrunelle/dowpy',
       author='JohnBrunelle',
       author_email='devjohnb@gmail.com',
       license='MIT',
       packages=['dowpy'],
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       install_requires=['requests'],
       test_suite='nose.collector',
       tests_require=['nose'],
